f21672e38569d1798aef6313c3d72af42ab80373
DONALDO\Donaldo <ldonaldo@uninorte.edu.co>
2020-09-09 11:22:26 -0500

    Updating repo

---
+++ models/notification.model.js
@@ -0,0 +1,26 @@
+const { Schema ,model} = require("mongoose");
+
+const notificationSchema = new Schema ({
+  lenderId:{
+    type:Schema.Types.ObjectId,
+    ref:"Lender"
+  },
+  tenantId:{
+    type:Schema.Types.ObjectId,
+    ref:"Tenant"
+  },
+  inventories:{
+    type: [{type: Schema.Types.ObjectId, ref: "Inventory"}]
+  },
+  events:{
+    type: [{type: Schema.Types.ObjectId, ref: "Event"}]
+  },
+  chats:{
+    type: Schema.Types.ObjectId, ref: "Chat"
+  }
+},{
+    timestamps:true
+})
+
+const notification = new model("Notification",notificationSchema)
+module.exports = notification
