d1c5da690725bb655587cfbc04fef370e6f4eb07
Diego-Martinez <d.ricardo2martinez@gmail.com>
2020-09-28 11:24:35 -0500

    corrected error

--- routes/notification.route.js
+++ routes/notification.route.js
@@ -3,8 +3,7 @@ const notificationServices = require ('../services/notifications.controller')
 const { authMiddleware } = require('../utils/middlewares')
 
 router.route('/').post(authMiddleware, notificationServices.createNotification)
-router.route('/').get(authMiddleware, notificationServices.getNotifications)
-router.route('/one').get(authMiddleware, notificationServices.getNotification)
+router.route('/').get(authMiddleware, notificationServices.getNotification)
 router.route('/').put(authMiddleware, notificationServices.updateNotification)
 
 module.exports = router
