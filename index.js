e1aa5f083396d3192e4dee0b6a70d80a6e9406a0
veilhelmAlex <veilhelm.alexander@gmail.com>
2020-10-01 10:12:55 -0500

    redeploy

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require("./app")
 const port = process.env.PORT || 4000
 
 app.listen( port, () => {
-    console.log(`listening at port ${port}`)
+    console.log(`listening ${port}`)
 }) 
