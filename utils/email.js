5dc25e52ed2a030b515ed36dff7aefa24b5cb0e3
veilhelmAlex <veilhelm.alexander@gmail.com>
2020-10-05 15:25:11 -0500

    fixed email redirection

--- utils/email.js
+++ utils/email.js
@@ -10,7 +10,7 @@ const sendReservationEmailToLender = async (lender, tenant, spaceTitle) => {
         from ${tenant.name}`,
     html: `<h4>Dear ${lender.name}, we are glad to tell you that your space ${spaceTitle} has a reservation request
     from ${tenant.name}</h4><br><br>
-    <strong>Checkout your Notifications center in <a href=${process.env.SHARED_BOX_URL}>www.SharedBox.com</a> where you would be able to accept or reject ${tenant.name}'s offer</strong>`,
+    <strong>Checkout your Notifications center in <a href=${process.env.SHARED_BOX_URL}notification>www.SharedBox.com</a> where you would be able to accept or reject ${tenant.name}'s offer</strong>`,
   }  
   await sgMail.send(mail)  
 }
@@ -33,7 +33,7 @@ const sendOfferRejectionEmailToTenant = async (titleSpace,tenant,nameLender) => 
     subject: `Hi ${tenant.name}!! your offer for the space${titleSpace} was rejected`,
     text: `Dear ${tenant.name}, we are sorry to tell you that your offer for the space ${titleSpace} was rejected by ${nameLender}`,
     html:`<h4>Dear ${tenant.name}, we are sorry to tell you that your offer for the space ${titleSpace} was rejected by ${nameLender}</h4><br><br>
-    <strong>Checkout your Notifications center in <a href=${process.env.SHARED_BOX_URL}>www.SharedBox.com</a> and don't worry we will help you to find the best space for you</strong>`,
+    <strong>Checkout your Notifications center in <a href=${process.env.SHARED_BOX_URL}notification>www.SharedBox.com</a> and don't worry we will help you to find the best space for you</strong>`,
   }  
   await sgMail.send(mail)  
 }
@@ -45,7 +45,7 @@ const sendOfferAcceptedEmailToTenant = async (titleSpace,initialDate,finalDate,t
     subject: `Hi ${tenant.name}!! your offer for the space${titleSpace} was Accepted!!`,
     text: `Dear ${tenant.name}, we are glad to tell you that your offer for the space ${titleSpace} was accepted by ${nameLender}`,
     html:`<h4>Dear ${tenant.name}, we are glad to tell you that your offer for the space ${titleSpace} was accepted by ${nameLender}</br>from ${initialDate} to ${finalDate} </h4><br><br>
-    <strong>Checkout your Notifications center in <a href=${process.env.SHARED_BOX_URL}>www.SharedBox.com</a> to see more information about your transaction</strong>`,
+    <strong>Checkout your Notifications center in <a href=${process.env.SHARED_BOX_URL}notification>www.SharedBox.com</a> to see more information about your transaction</strong>`,
   }  
   await sgMail.send(mail)  
 }
@@ -59,7 +59,7 @@ const sendPayResultEmailToTenant = async (titleSpace,initialDate,finalDate,tenan
     html:`<h4>Dear ${tenant.name}, we are glad to tell you that your transaction to rent the space ${titleSpace} was successfully taken by the Bank.
     </br>So from ${initialDate} to ${finalDate} the space is yours; congratulations!! be aware of your notifications on ${initialDate}, because the lender ${nameLender}
     is going to send you your inventory checklist.</h4><br><br>
-    <strong>Checkout your Notifications center in <a href=${process.env.SHARED_BOX_URL}>www.SharedBox.com</a> to see more information about your transaction</strong>`,
+    <strong>Checkout your Notifications center in <a href=${process.env.SHARED_BOX_URL}notification>www.SharedBox.com</a> to see more information about your transaction</strong>`,
   }  
   await sgMail.send(mail)  
 }
@@ -73,7 +73,7 @@ const sendReminder1DayBeforeReservationToLender = async (titleSpace,date,tenant,
     html:`<h4>Dear ${lender.name}, we are glad to tell that tomorrow your tenant ${tenant.name} will have access to your reserved space ${titleSpace}.
     </br>Remember what from ${date.initialDate} to ${date.finalDate} the space is for the tenant.
 
-    <strong>Checkout your Notifications center in <a href=${process.env.SHARED_BOX_URL}>www.SharedBox.com</a> to see more information about your transaction</strong>`,
+    <strong>Checkout your Notifications center in <a href=${process.env.SHARED_BOX_URL}notification>www.SharedBox.com</a> to see more information about your transaction</strong>`,
   }  
   await sgMail.send(mail)  
 }
@@ -86,7 +86,7 @@ const sendReminder1DayBeforeReservationToTenant = async (titleSpace,date,tenant,
     text: `Dear ${tenant.name}, we are glad to tell that tomorrow you will have access to your reserved space ${titleSpace} `,
     html:`<h4>Dear ${tenant.name}, we are glad to tell that tomorrow you will have access to your reserved space ${titleSpace}.
     </br>Remember what from ${date.initialDate} to ${date.finalDate} the space is yours; congratulations!!. If you have any problem please contact with the owner ${lender.name}.
-    <strong>Checkout your Notifications center in <a href=${process.env.SHARED_BOX_URL}>www.SharedBox.com</a> to see more information about your transaction</strong>`,
+    <strong>Checkout your Notifications center in <a href=${process.env.SHARED_BOX_URL}notification>www.SharedBox.com</a> to see more information about your transaction</strong>`,
   }  
   await sgMail.send(mail)  
 }
